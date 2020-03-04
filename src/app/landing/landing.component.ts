86f38281dab5c37c0262a91f29fad48867746662
Tonniva <tongiggabite@gmail.com>
2020-03-04 23:16:00 +0700

    up

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -11,6 +11,7 @@ export class LandingComponent implements OnInit {
   listdetail:any
   constructor(private apiService: RestService, private route: ActivatedRoute) { }
   ngOnInit(): void { 
+    
     window.location.replace("https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1653915528&redirect_uri=http://localhost:4200/home&state=12345abcde&scope=openid%20profile");
     
     
